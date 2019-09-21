1ea9a4a6e184d9e4e7a1e0eff9b319b3f5a84845
yyj <yeyajie1229@gmail.com>
2019-09-21 17:31:07 +0800

    Fix some bugs

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 config:
 	@emacs -Q --script etc/setup/setup-emacs.el
 
+tarball:
+	cd ~ && tar zcf /tmp/emacs.d.tgz --exclude-vcs-ignores .emacs.d
+
 private:
 	git clone https://github.com/manateelazycat/awesome-pair ~/.emacs.d/private/awesome-pair
 	git clone https://github.com/emacsmirror/dired-plus ~/.emacs.d/private/dired-plus
--- init.el
+++ init.el
@@ -6,6 +6,8 @@
   (expand-file-name "var" user-emacs-directory)
   "All data and external executable file in this direcotry")
 
+(defvar emacs-lite-setup-p (getenv "EMACS_LITE"))
+
 ;; Add `emacs-config-directory' to `load-path'
 (add-to-list 'load-path emacs-config-directory)
 
@@ -27,31 +29,33 @@
 ;; ----------------------------------------
 
 ;; All packages required in this section are defined in their init files
-(require 'init-org)
-(require 'init-notes)
-(require 'init-jupyter)
 
 (when (or env-has-aspell-p env-has-hunspell-p)
   (require 'init-spelling))
 (when env-has-latex-p
-  (require 'init-latex))
+  (require 'init-latex)
+  (require 'init-org)
+  (require 'init-notes)
+  (require 'init-jupyter))
 (when env-has-git-p
   (require 'init-git))
 (when env-has-gtags-p
   (require 'init-tags))
 
 ;; Programming modes
+(unless emacs-lite-setup-p
+  (require 'init-cpp)
+  (require 'init-haskell)
+  (require 'init-web)
+  (require 'init-rust)
+  (require 'init-java)
+  (require 'init-idris))
+
 (require 'init-lsp-mode)
-(require 'init-cpp)
-(require 'init-haskell)
+(require 'init-perl5)
 (require 'init-python)
 (require 'init-lisp)
 (require 'init-sh)
-(require 'init-web)
-(require 'init-rust)
-(require 'init-java)
-(require 'init-perl5)
-(require 'init-idris)
 
 ;; Some keybindings using when editing
 (require 'init-editing)
@@ -67,6 +71,7 @@
   (require 'init-emms))
 
 (require 'init-extra)
-(require 'init-tabnine)
-(require 'init-play)
+(unless emacs-lite-setup-p
+  (require 'init-tabnine)
+  (require 'init-play))
 ;; (require 'init-treemacs)
