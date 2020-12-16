aa899f907eefb5270551f4c37e27a41cc7ed75c4
yyj <yeyajie1229@gmail.com>
2020-12-16 11:12:58 +0800

    Fix typos

--- lisp/sh/hook.el
+++
@@ -1,7 +0,0 @@
-;;; -*- lexical-binding: t; -*-
-
-(define-hook! ymacs-sh|setup (sh-mode-hook)
-  (when (buffer-enable-rich-feature-p)
-    (try-enable-lsp! sh)))
-
-(add-hook 'makefile-mode-hook #'whitespace-mode)
