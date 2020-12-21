8d3e05d89d5400d4bf94c3d1de6433b4f7787e2c
yyj <yeyajie1229@gmail.com>
2020-12-21 11:22:03 +0800

    fixup! Update

--- lisp/default/hooks.el
+++ lisp/default/hooks.el
@@ -99,7 +99,7 @@
   (midnight-mode 1)
   ;; (display-time-mode 1)
   (transient-mark-mode 1)
-  ;; (delete-selection-mode 1)
+  (delete-selection-mode 1)
   ;; Auto save to files
   ;; (auto-save-visited-mode 1)
 
